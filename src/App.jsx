34b690a9f82570fedb65d63bc3b57c281fc6ecb5
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-22 16:39:52 +0100

    Colecting user inputs with refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
-  return (
-    <main className="h-screen my-8 flex flex-gap-8">
-        <ProjectsSidebar />
-        <NoProjectSelected />
-        {/*<NewProject />*/}
-    </main>
-  );
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        project: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        });
+    }
+
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const newProject = {
+                ...projectData,
+                id: Math.random()
+            }
+
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                project: [...prevState.project, newProject],
+            }
+        });
+    }
+
+    let content;
+    if(projectsState.selectedProjectId === null){
+        content = (<NewProject onAdd={handleAddProject} />);
+
+    }else if(projectsState.selectedProjectId === undefined){
+        content = (<NoProjectSelected onStartAddProject={handleStartAddProject} />);
+    }
+
+    console.log(projectsState);
+
+    return (
+        <main className="h-screen my-8 flex flex-gap-8">
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+            {content}
+        </main>
+    );
 }
 
 export default App;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,24 @@
-export default function Input({ label, isTextarea, ...rest }) {
-    return <p className="flex flex-col gap-1 my-4">
-        <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-        {
-            isTextarea ?
-            <textarea
-                className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-                {...rest} >
+import {forwardRef} from 'react';
+
+const Input = forwardRef(
+    function Input({ label, isTextarea, ...rest }, ref) {
+        return <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            {
+                isTextarea ?
+                    <textarea
+                        ref={ref}
+                        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+                        {...rest} >
             </textarea>
-                : <input
-                    className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-                    {...rest}
-                />
-        }
-    </p>;
-}
+                    : <input
+                        ref={ref}
+                        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+                        {...rest}
+                    />
+            }
+        </p>;
+    }
+);
+
+export default Input;
